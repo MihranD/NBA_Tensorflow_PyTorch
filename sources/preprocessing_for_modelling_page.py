905defc094e4a5003178182761984a15efcc583e
MihranD <mihrandovlatyan@gmail.com>
2024-04-30 09:48:10 +0200

    finish preprocessing for modelling

--- sources/preprocessing_for_modelling_page.py
+++ sources/preprocessing_for_modelling_page.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import numpy as np
+from joblib import dump, load
+from sklearn.model_selection import train_test_split
 from sources.utils import read_df
 
 def show_preprocessing_for_modelling_purposes_page():
@@ -32,16 +34,131 @@ In summary, the process of categorical variable transformation and feature norma
               ''')
   
   # Transform attributes with high cardinality
-  transform_attributes_with_high_cardinality(df)
+  df = transform_attributes_with_high_cardinality(df)
 
   # Transform other categorical attributes
-  transform_other_categorical_attributes(df)
+  df = transform_other_categorical_attributes(df)
 
   # Transform quantitative attributes, which have unique id values
-  transform_quantitative_attributes(df)
+  df = transform_quantitative_attributes(df)
 
   # Transform date attribute
-  transform_date_attribute(df)
+  df = transform_date_attribute(df)
+
+  # Show Correlation matrix
+  show_correlation_matrix(df)
+
+  # Split train and test parts
+  split_train_and_test_parts(df)
+  save_train_test_set(df)
+
+  # Conclusion for preprocessing
+  conclusion_for_preprocessing(df)
+
+# Conclusion for preprocessing
+def conclusion_for_preprocessing(df):
+  st.write("#### Conclusion for preprocessing")
+  st.write("We converted all categorical attributes to quantitative. Our dataset ready for further modeling.")
+  if st.checkbox("Show preprocessed dataset"):
+    st.dataframe(df.dtypes)
+  st.markdown('''
+For now, our focus has been on converting categorical attributes into quantitative representations. We'll defer the scaling process to the appropriate modeling stage, where each model can adopt its own scaling approach (Standardization, MinMax Scaling or other type of scaling) based on its requirements and the characteristics of the data. The current emphasis has been on ensuring that our data is prepared in a format conducive to modeling, with further considerations such as feature scaling left for subsequent stages tailored to each specific model.
+              ''')
+  
+# Save train-test sets in a file
+def save_train_test_set(df):
+  data = df.drop('Shot Made Flag', axis = 1)
+  target = df['Shot Made Flag']
+  X_train, X_test, y_train, y_test = train_test_split(data, target, test_size = 0.2, random_state = 66)
+  # WARNING: This call can take a few minutes.
+  # Save the data
+  if st.button("Save data in joblib file"):
+    dump((X_train, X_test, y_train, y_test), 'NBA Shot Locations 1997 - 2020-Report2-train-test.joblib')
+    st.write("New joblib file generated successfully.")
+    # Later model parts can read the data from it with help of: 
+    # X_train, X_test, y_train, y_test = load('NBA Shot Locations 1997 - 2020-Report2-train-test.joblib')
+
+# Split train and test parts
+def split_train_and_test_parts(df):
+  st.write("#### Split train and test parts")
+  st.write("Let's create a data DataFrame in which we will store the features and create the target ('Shot Made Flag') data variable.")
+  st.markdown('''
+In order to test the performance of the classification model, it is necessary to select a part of the data that is dedicated to the evaluation and that is therefore not taken into account in the training of the model.
+To do this, the data must be systematically divided into a training set (X_train and y_train) and a test set (X_test and y_test).
+
+Usually, the test set size is between 15% and 30% of the total amount of data. The choice of the distribution depends mainly on the quantity and quality of the available data. We will choose 20%.
+
+Let's randomly divide the matrices into a training set and a test set corresponding to 80% and 20% of the total amount of available data respectively. Add the argument random_state = 66 for randomness reproducibility.
+              ''')
+  st.write("To build a classification model, we need to train our model on the training set only.")
+  st.markdown('''
+We intend to store these training and testing sets in a file for later use in modeling tasks. Ensuring consistency, all models will be trained on these identical sets. This approach will provide us with a clearer understanding of each model's performance under the same conditions, aiding in the determination of the most effective model.
+              ''')
+
+# Calculate the correlation matrix
+@st.cache_data
+def correlation_matrix(df):
+  return df.corr()
+
+# Get the correlation of features with the target variable
+@st.cache_data
+def target_correlation(corr_matrix):
+  return corr_matrix['Shot Made Flag'].abs().sort_values(ascending=False)
+
+# Show Correlation matrix
+def show_correlation_matrix(df):
+  st.write("#### Correlation matrix")
+  st.write("Now that we have transformed all features into numeric values, we can proceed to create a correlation matrix specifically for the 'Shot Made Flag' target variable.")
+
+  # Calculate the correlation matrix
+  corr_matrix = correlation_matrix(df)
+  # Get the correlation of features with the target variable
+  target_corr = target_correlation(corr_matrix)
+  
+  if st.checkbox("Top Ten Correlated Features Heatmap"):
+    # Get the top ten most correlated features with the target variable
+    top_corr_features = target_corr[1:11].index  # Exclude the target itself
+
+    # Select only the top ten most correlated features from the correlation matrix
+    corr_matrix_top = corr_matrix.loc[top_corr_features, top_corr_features]
+
+    # Plot the heatmap
+    fig = plt.figure(figsize=(10, 8))  # Adjust the figure size as needed
+    sns.heatmap(corr_matrix_top, annot=True, cmap='coolwarm', fmt=".2f")
+    plt.title('Top Ten Correlated Features Heatmap')
+    st.pyplot(fig)
+
+    st.markdown('''
+One can see by viewing these map that for an NBA player to increase his chances of making a shot, he should be as close as possible to the basket (ShotZoneRange_Less Than 8 ft., ShotZoneBasic_Restricted Area). 
+
+The high correlation coefficient of 0.99 between 'ShotType_3PT Field Goal' and 'ShotZoneRange_24+ ft.' signifies a strong relationship between attempting three-point shots and shooting from beyond the 24-foot mark on the court. This suggests that NBA players who frequently attempt three-pointers are more likely to do so from long distances, beyond the traditional three-point line. Coaches and analysts may use this insight to strategize offensive plays that leverage players' shooting abilities from long range, optimizing scoring opportunities based on shot selection and player positions on the court.
+                ''')
+    st.write("---")
+
+  if st.checkbox("Correlation of Features with 'Shot Made Flag'"):
+    features = target_corr.index[1:16].tolist()  # Exclude 'Shot Made Flag' and get the first 15 features
+    correlations = target_corr.values[1:16].tolist()  # Exclude its correlation value and get the first 15 correlations
+
+    # Plot the bar plot with adjusted spacing and grid lines for the first 15 features
+    fig = plt.figure(figsize=(10, len(features) * 0.5))  # Adjust figure height based on number of features
+    bars = plt.barh(features, correlations, color='skyblue')
+    plt.xlabel('Correlation')
+    plt.title('Correlation of Features with "Shot Made Flag" (First 15 Features)')
+    plt.gca().invert_yaxis()  # Invert y-axis to display highest correlation at the top
+    plt.grid(True, linestyle='--', alpha=0.6)  # Add grid lines with dashed style and transparency
+    plt.tight_layout()  # Adjust spacing to prevent text overlap
+    st.pyplot(fig)
+
+    st.markdown('''
+The correlation plot highlights the features most strongly correlated with 'Shot Made Flag'.
+
+'Action Type Frequency' shows a high positive correlation, suggesting that the frequency of specific actions during a shot attempt significantly impacts shot success.
+
+'ShotZoneBasic_Restricted Area' and 'Shot Distance' also exhibit strong positive correlations, indicating that shots taken from restricted areas and shorter distances are more likely to be made.
+
+Furthermore, 'ShotType_2PT Field Goal' and 'ShotType_3PT Field Goal' reflect the influence of shot type on success rates, with both two-point field goals (2PT) and three-point field goals (3PT) having an equal impact on shot success rates.
+                ''')
+    st.write("---")
 
 # Transform date attribute
 def transform_date_attribute(df):
@@ -71,6 +188,7 @@ There are some common approaches to feature engineering with date data in classi
 
   st.write("Finally, we remove 'Game Date' column.")
   df.drop('Game Date', axis = 1, inplace = True)
+  return df
 
 # Transform quantitative attributes, which have unique id values
 @st.cache_data
@@ -98,6 +216,7 @@ Frequency encoding can indeed be applied to our unique identifiers.
 
   # Drop the original columns
   df.drop(['Game ID', 'Game Event ID', 'Player ID'], axis = 1, inplace = True)
+  return df
 
 # Transform other categorical attributes
 @st.cache_data
@@ -118,6 +237,7 @@ def transform_other_categorical_attributes(df):
   df = pd.concat([df, shot_type_encoded, shot_zone_basic_encoded, shot_zone_area_encoded, season_zone_range_encoded, season_type_encoded], axis=1)
   st.write("Step 3. Drop the original categorical columns")
   df.drop(['Shot Type', 'Shot Zone Basic', 'Shot Zone Area', 'Shot Zone Range', 'Season Type'], axis = 1, inplace = True)
+  return df
 
 # Transform attributes with high cardinality
 def transform_attributes_with_high_cardinality(df):
@@ -165,6 +285,7 @@ These values represent the proportions of each category in the dataset, where th
   # Drop the original categorical columns
   df.drop(['Team Name', 'Home Team', 'Away Team'], axis = 1, inplace = True)
   st.write("In our dataset 'Team ID' and 'Team Name' contains exactly the same information, so we can remove 'Team ID' column.")
+  return df
 
 # Function to perform frequency encoding
 def frequency_encode_column(df, column_name):
